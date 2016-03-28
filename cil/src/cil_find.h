da97f7a7ce8f7668da00bf1aceeb840c76c36c10
Laurent Bigonville <bigon@bigon.be>
2016-03-28 16:41:13 +0200

    Imported Upstream version 2.5

--- cil/src/cil_find.h
+++ cil/src/cil_find.h
@@ -1,16 +1,16 @@
 /*
  * Copyright 2011 Tresys Technology, LLC. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *    1. Redistributions of source code must retain the above copyright notice,
  *       this list of conditions and the following disclaimer.
- * 
+ *
  *    2. Redistributions in binary form must reproduce the above copyright notice,
  *       this list of conditions and the following disclaimer in the documentation
  *       and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY TRESYS TECHNOLOGY, LLC ``AS IS'' AND ANY EXPRESS
  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
@@ -21,97 +21,20 @@
  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * The views and conclusions contained in the software and documentation are those
  * of the authors and should not be interpreted as representing official policies,
  * either expressed or implied, of Tresys Technology, LLC.
  */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
+#include "cil_flavor.h"
+#include "cil_tree.h"
+#include "cil_list.h"
 
-#include "cil_log.h"
+#ifndef CIL_FIND_H_
+#define CIL_FIND_H_
 
-__attribute__((noreturn)) void cil_default_mem_error_handler(void)
-{
-	cil_log(CIL_ERR, "Failed to allocate memory\n");
-	exit(1);
-}
+int cil_find_matching_avrule_in_ast(struct cil_tree_node *current, enum cil_flavor flavor, void *target, struct cil_list *matching, int match_self);
+struct cil_list *cil_expand_class(struct cil_class *class);
 
-void (*cil_mem_error_handler)(void) = &cil_default_mem_error_handler;
-
-void cil_set_mem_error_handler(void (*handler)(void))
-{
-	cil_mem_error_handler = handler;
-}
-
-void *cil_malloc(size_t size)
-{
-	void *mem = malloc(size);
-	if (mem == NULL){
-		if (size == 0) {
-			return NULL;
-		}
-		(*cil_mem_error_handler)();
-	}
-
-	return mem;
-}
-
-void *cil_calloc(size_t num_elements, size_t element_size)
-{
-	void *mem = calloc(num_elements, element_size);
-	if (mem == NULL){
-		(*cil_mem_error_handler)();
-	}
-
-	return mem;
-}
-
-void *cil_realloc(void *ptr, size_t size)
-{
-	void *mem = realloc(ptr, size);
-	if (mem == NULL){
-		if (size == 0) {
-			return NULL;
-		}
-		(*cil_mem_error_handler)();
-	}
-
-	return mem;
-}
-
-
-char *cil_strdup(const char *str)
-{
-	char *mem = NULL;
-
-	if (str == NULL) {
-		return NULL;
-	}
-
-	mem = strdup(str);
-	if (mem == NULL) {
-		(*cil_mem_error_handler)();
-	}
-
-	return mem;
-}
-
-__attribute__ ((format (printf, 2, 3))) int cil_asprintf(char **strp, const char *fmt, ...)
-{
-	int rc;
-	va_list ap;
-
-	va_start(ap, fmt);
-	rc = vasprintf(strp, fmt, ap);
-	va_end(ap);
-
-	if (rc == -1) {
-		(*cil_mem_error_handler)();
-	}
-
-	return rc;
-}
+#endif
